e6bc2726898b0ae1e47fc89a0fd869e6e5600616
Forrest Cranmer <letosleepingdragon@gmail.com>
2017-11-15 09:36:44 +0800

    added top rest. page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    collection do
+      get 'top', to:"restaurants#top"
+    end
+  end
+
+      get 'restaurants/top', to: 'restaurants#top'
+  root to: "restaurants#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Restaurant.create(name: Faker::Hipster.word, address: Faker::Address.street_name, description: Faker::Hipster.sentence, stars: Faker::Number.between(1,5))
+end
